8ee0a81529ecc8d37344a4f86620b1a6a75bb1cb
Younes Halim <younes.halim@gmail.com>
2024-05-20 15:30:47 -0400

    update game class

--- game_sys/game_sys.py
+++ game_sys/game_sys.py
@@ -4,7 +4,7 @@ from pygame._sprite import LayeredUpdates
 from pygame.time import Clock
 
 from game_objects.background import Background
-from game_objects.t_rex import TRex
+from game_objects.cactus import Cactus
 from game_sys import assets
 from game_sys.settings import GameSettings
 
@@ -16,14 +16,24 @@ class Game:
         self.__clock = pygame.time.Clock()
         self.__sprites = LayeredUpdates()
         self.init_game()
-        pygame.display.set_caption('Asteroid Survivor Simulation')
-        pygame.display.set_icon(assets.get_sprite('game_icon'))
+        self.cacti = [Cactus(self.sprites)]
 
     def init_game(self):
-        TRex(self.sprites)
-        Background(self.sprites)
+        assets.load_sprites()
+        Background(0, self.sprites)
+        Background(1, self.sprites)
+        pygame.display.set_caption('T-Rex AI')
+        pygame.display.set_icon(assets.get_sprite('game_icon'))
 
-        pass
+    def draw_cactus(self):
+        if self.cacti:
+            last_cactus = self.cacti[-1]
+            if not last_cactus.alive() and last_cactus.rect.left < 0:
+                cactus = Cactus(self.sprites)
+                cactus.vel = last_cactus.vel
+                cactus.vel.x -= .25
+                self.cacti.append(cactus)
+                self.cacti.remove(last_cactus)
 
     @property
     def sprites(self) -> LayeredUpdates:
