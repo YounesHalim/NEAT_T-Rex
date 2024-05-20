e8747aa1dd353276eac416dac33e3a69d93753ad
Younes Halim <younes.halim@gmail.com>
2024-05-20 15:29:25 -0400

    update background.py

--- game_objects/background.py
+++ game_objects/background.py
@@ -1,4 +1,5 @@
 import pygame.sprite
+from pygame import Vector2
 from pygame._sprite import AbstractGroup
 
 from game_sys import assets
@@ -7,14 +8,17 @@ from game_sys.settings import GameSettings
 
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self, *groups: AbstractGroup):
+
+    def __init__(self, index: int, *groups: AbstractGroup):
         self._layer = Layer.BACKGROUND
-        self.sprite = assets.get_sprite('')
-        self.scale = pygame.transform.scale(self.sprite,
-                                            (GameSettings.SCREEN_WIDTH.value, GameSettings.SCREEN_HEIGHT.value))
-        self.image = self.scale
-        self.rect = self.image.get_rect(topleft=())
+        self.image = pygame.transform.scale(assets.get_sprite('bg'),
+                                            (GameSettings.SCREEN_WIDTH.value,
+                                             GameSettings.SCREEN_HEIGHT.value))
+        self.rect = self.image.get_rect(topleft=(GameSettings.SCREEN_WIDTH.value * index, 0))
+        self.vel = Vector2(-2, 0)
         super().__init__(*groups)
 
     def update(self, *args, **kwargs):
-        pass
+        self.rect.move_ip(self.vel)
+        if self.rect.right <= 0:
+            self.rect.x = GameSettings.SCREEN_WIDTH.value
