d5af16d91cccaf102b05db74412735031b18b968
Younes Halim <younes.halim@gmail.com>
2024-05-21 15:17:37 -0400

    update game class

--- game_sys/game_sys.py
+++ game_sys/game_sys.py
@@ -5,7 +5,9 @@ from pygame.time import Clock
 
 from game_objects.background import Background
 from game_objects.cactus import Cactus
+from game_objects.ground import Ground
 from game_sys import assets
+from game_sys.layer import Layer
 from game_sys.settings import GameSettings
 
 
@@ -16,20 +18,26 @@ class Game:
         self.__clock = pygame.time.Clock()
         self.__sprites = LayeredUpdates()
         self.init_game()
-        self.cacti = [Cactus(self.sprites)]
+        self.cacti = [Cactus(self.__sprites)]
 
     def init_game(self):
         assets.load_sprites()
-        Background(0, self.sprites)
-        Background(1, self.sprites)
+        for i in range(0, 2):
+            Background(i, self.__sprites)
+            Ground(i, self.__sprites)
+
         pygame.display.set_caption('T-Rex AI')
         pygame.display.set_icon(assets.get_sprite('game_icon'))
 
-    def draw_cactus(self):
+    def draw_cactus(self, genomes: list = None):
         if self.cacti:
             last_cactus = self.cacti[-1]
-            if not last_cactus.alive() and last_cactus.rect.left < 0:
-                cactus = Cactus(self.sprites)
+            if not last_cactus.alive():
+                ui = self.sprites.get_sprites_from_layer(Layer.UI)[0]
+                ui.score += 1
+                # for genome in genomes:
+                #     genome.fitness += 5
+                cactus = Cactus(self.__sprites)
                 cactus.vel = last_cactus.vel
                 cactus.vel.x -= .25
                 self.cacti.append(cactus)
